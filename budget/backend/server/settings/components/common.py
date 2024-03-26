80a921b352c6822bc9a58c362ac09209dd0cf597
VF <77busina@gmail.com>
2024-03-26 17:12:54 +0400

    refactor: some settings, such as common.py

--- budget/backend/server/settings/components/common.py
+++ budget/backend/server/settings/components/common.py
@@ -65,7 +65,7 @@ SPECTACULAR_SETTINGS = {
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": ["backend/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
